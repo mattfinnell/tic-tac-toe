bfa7c5453361a9c0fa938a78107ed5efae0ff707
Matt Finnell <mattfinnell104@gmail.com>
2019-02-01 16:50:40 -0800

    made a rough implementation

--- TicTacToe/Solver.py
+++ TicTacToe/Solver.py
@@ -2,5 +2,52 @@ class Solver(object):
     def __init__(self):
         pass
     
-    def solve(self, game_board):
-        return False
+    def getWinner(self, game_board):
+        case_table = {
+            0 : "0",
+            1 : "X",
+            None: "<tie>"
+        }
+
+        result = self._getWinner(game_board)
+
+        return case_table[result]
+    
+    def _getWinner(self, game_board):
+        row_winner = self._row_winner(game_board)
+        if row_winner:
+            return row_winner
+
+        column_winner = self._column_winner(game_board)
+        if column_winner:
+            return column_winner
+
+        diagonal_winner = self._diagonal_winner(game_board)
+        if diagonal_winner:
+            return diagonal_winner
+
+        return None
+
+    def _row_winner(self, game_board):
+        for row in game_board:
+            if row[0] == row[1] == row[2]:
+                # Must return the winning_character
+                return row[0]
+
+        return None 
+
+    def _column_winner(self, game_board):
+        for i in range(3):
+            if game_board[0][i] == game_board[1][i] == game_board[2][i]:
+                return game_board[0][i]
+        
+        return None
+
+    def _diagonal_winner(self, game_board):
+        right_left_diagonal = game_board[0][0] == game_board[1][1] == game_board[2][2]
+        left_right_diagonal = game_board[2][2] == game_board[1][1] == game_board[0][0]
+
+        if right_left_diagonal or left_right_diagonal:
+            return game_board[1][1]
+        
+        return None
--- test/test_tic_tac_toe.py
+++ test/test_tic_tac_toe.py
@@ -2,6 +2,47 @@ from TicTacToe.Solver import Solver
 
 import pytest
 
+def test_solver_on_example_board(solver, example_board):
+    result = solver._getWinner(example_board)
+
+    assert result == 1
+
+def test_solver_returning_player_string_given_valid_board(solver, example_board):
+    assert solver.getWinner(example_board) == "X"
+
+def test_solver_returning_tie_given_invalid_board(solver, invalid_board):
+    assert solver.getWinner(invalid_board) == "<tie>"
+
+def test_column_solver_on_valid_columns(solver, board_valid_column):
+    result = solver._column_winner(board_valid_column)
+
+    assert result == 0
+
+def test_column_solver_on_invalid_columns(solver, invalid_board):
+    result = solver._column_winner(invalid_board) 
+
+    assert not result
+
+def test_row_solver_on_valid_rows(solver, board_valid_row):
+    result = solver._row_winner(board_valid_row)
+
+    assert result == 1
+
+def test_row_solver_on_invalid_rows(solver, invalid_board):
+    result = solver._row_winner(invalid_board) 
+
+    assert not result
+
+def test_diagonal_solver_on_valid_diagonals(solver, board_valid_diagonal):
+    result = solver._diagonal_winner(board_valid_diagonal)
+
+    assert result == 1
+
+def test_diagonal_solver_on_invalid_diagonals(solver, invalid_board):
+    result = solver._diagonal_winner(invalid_board) 
+
+    assert not result
+
 @pytest.fixture
 def solver():
     return Solver()
@@ -16,7 +57,7 @@ def invalid_board():
 
 @pytest.fixture
 def board_valid_row():
-    return [[0, 0, 0],[1, 1, 1],[0, 0, 0]]
+    return [[1, 0, 0],[1, 1, 1],[0, 1, 0]]
 
 @pytest.fixture
 def board_no_valid_row():
@@ -24,7 +65,7 @@ def board_no_valid_row():
 
 @pytest.fixture
 def board_valid_column():
-    return [[0, 1, 0],[0, 1, 0],[0, 1, 0]]
+    return [[0, 1, 0],[0, 0, 0],[0, 1, 0]]
 
 @pytest.fixture
 def board_no_valid_column():
@@ -37,6 +78,3 @@ def board_valid_diagonal():
 @pytest.fixture
 def board_no_valid_diagonal():
     return invalid_board()
-
-def test_solver_on_example_board(solver, example_board):
-    assert solver.solve(example_board)
